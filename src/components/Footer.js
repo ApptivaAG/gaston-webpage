4720cf1832a7e6022396e9ff6981853ce7cc8247
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2019-09-23 13:33:21 +0200

    Added language switch

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import styled from 'styled-components'
 import { injectIntl } from 'gatsby-plugin-intl'
 
@@ -8,7 +9,12 @@ const FooterStyle = styled.footer`
   color: ${p => p.theme.primary};
 
   @media (min-width: 600px) {
+    display: flex;
+    justify-content: space-between;
     padding: 0 2rem;
+  }
+  .active {
+    font-weight: bold;
   }
 `
 
@@ -18,6 +24,15 @@ const Footer = ({ intl }) => (
       © 2019 • Gaston • {`${intl.formatMessage({ id: 'footer.by' })} `}
       <a href="https://apptiva.ch">Apptiva</a> •{' '}
       {intl.formatMessage({ id: 'footer.switzerland' })}
+    </p>
+    <p>
+      <Link to="en" activeClassName="active" partiallyActive>
+        en
+      </Link>
+      &nbsp;
+      <Link to="de" activeClassName="active" partiallyActive>
+        de
+      </Link>
     </p>
   </FooterStyle>
 )
