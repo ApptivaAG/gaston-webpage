24d26774eedfe7ab66be46013820d346fb08f1a3
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2019-12-12 11:34:34 +0100

    Render smaller team images

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,54 +1,16 @@
 import React from 'react'
-import { FormattedMessage,FormattedHTMLMessage } from 'gatsby-plugin-intl'
-import styled from "styled-components"
+import { FormattedMessage, FormattedHTMLMessage } from 'gatsby-plugin-intl'
+import styled from 'styled-components'
+import { useStaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
 
 import Layout from '../components/Layout'
 import SEO from '../components/SEO'
 import Container from '../styles/Container'
-import theme from "../styles/theme"
-
-import linus from "../images/linus-huesler-prev.jpg"
-import markus from "../images/markus-tanner-prev.jpg"
-import patrik from "../images/patrik-stutz-prev.jpg"
-import philip from "../images/philip-schoenholzer-prev.jpg"
-import renato from "../images/renato-wasescha-prev.jpg"
-import roman from "../images/roman-schaller-prev.jpg"
-import david from "../images/david-decker-prev.jpg"
-
-const employees = [
-  {
-    name: "Linus Huesler",
-    position: "software-developer",
-    image: linus
-  },{
-    name: "Markus Tanner",
-    position: "software-developer",
-    image: markus
-  },{
-    name: "Patrik Stutz",
-    position: "software-developer",
-    image: patrik
-  },{
-    name: "Philip Schoenholzer",
-    position: "software-developer",
-    image: philip
-  },{
-    name: "Renato Wasescha",
-    position: "software-developer",
-    image: renato
-  },{
-    name: "Roman Schaller",
-    position: "software-developer",
-    image: roman
-  },{
-    name: "David Decker",
-    position: "trainee",
-    image: david
-  }
-]
+import theme from '../styles/theme'
 
 const EmployeeList = styled.div`
-  margin-top:60px;
+  margin-top: 60px;
   display: flex;
   flex-wrap: wrap;
   justify-content: space-around;
@@ -56,24 +18,23 @@ const EmployeeList = styled.div`
 const EmployeeContainer = styled.div`
   width: 12rem;
   margin: 20px;
-  text-align:center;
+  text-align: center;
 `
-const EmployeeImage = styled.img`
+const EmployeeImage = styled(Img)`
   width: 100%;
-  border-radius:1000px;
-  
+  border-radius: 1000px;
+
   box-shadow: 0 0 10px 0px #ccc;
 `
 const EmployeeName = styled.h3`
   color: ${theme.primary};
-  margin-bottom:0px;
+  margin-bottom: 0px;
 `
-const EmployeePosition = styled.span`
-`
+const EmployeePosition = styled.span``
 
-const Employee = ({employee:{name,position,image}})=>(
+const Employee = ({ employee: { name, position, image } }) => (
   <EmployeeContainer>
-    <EmployeeImage src={image} />
+    <EmployeeImage fixed={image.childImageSharp.fixed} />
     <EmployeeName>{name}</EmployeeName>
     <EmployeePosition>
       <FormattedMessage id={`about.${position}`} />
@@ -81,17 +42,119 @@ const Employee = ({employee:{name,position,image}})=>(
   </EmployeeContainer>
 )
 
-export default ()=>{
+export default () => {
+  const {
+    david,
+    linus,
+    markus,
+    patrik,
+    philip,
+    renato,
+    roman,
+  } = useStaticQuery(graphql`
+    query {
+      david: file(relativePath: { eq: "team/david-decker-prev.jpg" }) {
+        childImageSharp {
+          fixed(width: 192) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+      linus: file(relativePath: { eq: "team/linus-huesler-prev.jpg" }) {
+        childImageSharp {
+          fixed(width: 192) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+      markus: file(relativePath: { eq: "team/markus-tanner-prev.jpg" }) {
+        childImageSharp {
+          fixed(width: 192) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+      patrik: file(relativePath: { eq: "team/patrik-stutz-prev.jpg" }) {
+        childImageSharp {
+          fixed(width: 192) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+      philip: file(relativePath: { eq: "team/philip-schoenholzer-prev.jpg" }) {
+        childImageSharp {
+          fixed(width: 192) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+      renato: file(relativePath: { eq: "team/renato-wasescha-prev.jpg" }) {
+        childImageSharp {
+          fixed(width: 192) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+      roman: file(relativePath: { eq: "team/roman-schaller-prev.jpg" }) {
+        childImageSharp {
+          fixed(width: 192) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `)
+
+  const employees = [
+    {
+      name: 'Linus Huesler',
+      position: 'software-developer',
+      image: linus,
+    },
+    {
+      name: 'Markus Tanner',
+      position: 'software-developer',
+      image: markus,
+    },
+    {
+      name: 'Patrik Stutz',
+      position: 'software-developer',
+      image: patrik,
+    },
+    {
+      name: 'Philip Schoenholzer',
+      position: 'software-developer',
+      image: philip,
+    },
+    {
+      name: 'Renato Wasescha',
+      position: 'software-developer',
+      image: renato,
+    },
+    {
+      name: 'Roman Schaller',
+      position: 'software-developer',
+      image: roman,
+    },
+    {
+      name: 'David Decker',
+      position: 'trainee',
+      image: david,
+    },
+  ]
+
   return (
     <Layout>
       <SEO title="about us" />
       <Container>
-        <h2><FormattedMessage id="header.about" /></h2>
+        <h2>
+          <FormattedMessage id="header.about" />
+        </h2>
         <p>
           <FormattedHTMLMessage id="about.content" />
         </p>
         <EmployeeList>
-          {employees.map(employee=>(
+          {employees.map(employee => (
             <Employee employee={employee} />
           ))}
         </EmployeeList>
